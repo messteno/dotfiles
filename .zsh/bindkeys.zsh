0e85f98ee6757c6ebaa4575f788a525b4918b3b3
Artem Meshcheryakov <messteno@gmail.com>
2014-01-02 16:11:02 +0400

    Add zsh dotfiles

---
+++ .zsh/bindkeys.zsh
@@ -0,0 +1,16 @@
+# bindkey '^[[1;5C' emacs-forward-word
+# bindkey '^[[1;5D' emacs-backward-word
+# bindkey "^[OB" down-line-or-search
+# bindkey "^[OC" forward-char
+# bindkey "^[OD" backward-char
+# bindkey "^[OF" end-of-line
+# bindkey "^[OH" beginning-of-line
+# bindkey "^[[1~" beginning-of-line
+# bindkey "^[[3~" delete-char
+# bindkey "^[[4~" end-of-line
+# bindkey "^[[5~" up-line-or-history
+# bindkey "^[[6~" down-line-or-history
+# bindkey "^?" backward-delete-char
+# bindkey '^[[Z' reverse-menu-complete
+
+
---
+++ .zsh/setopt.zsh
@@ -0,0 +1,25 @@
+setopt APPEND_HISTORY
+#setopt HIST_IGNORE_ALL_DUPS
+#setopt HIST_IGNORE_SPACE
+#setopt HIST_REDUCE_BLANKS
+#setopt histexpiredupsfirst histfindnodups
+#setopt histignoredups histnostore histverify extended_history share_history
+
+
+# Main settings
+setopt NO_BEEP
+setopt AUTO_CD
+setopt CORRECT_ALL
+#setopt IGNORE_EOF
+#setopt notify globdots correct pushdtohome cdablevars autolist
+#setopt correctall autocd recexact longlistjobs
+#setopt autoresume histignoredups pushdsilent noclobber
+#setopt autopushd pushdminus extendedglob rcquotes mailwarning
+#setopt noflowcontrol
+#unsetopt bgnice autoparamslash
+#typeset -U path cdpath fpath manpath
+#READNULLCMD=${PAGER}
+#unsetopt IGNORE_EOF # выход по Ctrl-D
+
+
+
