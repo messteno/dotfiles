c75645f4f0f3c70db638947802da333c5717f9b5
Artem Meshcheryakov <messteno@gmail.com>
2015-11-05 23:12:55 +0300

    Fix zsh cache path

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -12,7 +12,7 @@ zstyle ":completion:*:commands" rehash 1
 
 # Use cache
 zstyle ':completion:*' use-cache on
-zstyle ':completion:*' cache-path ~/.zsh/cache
+zstyle ':completion:*' cache-path ~/.cache/zsh
 
 # Version control completion
 zstyle ':vcs_info:*' actionformats '%F{5}(%f%s%F{5})%F{3}-%F{5}[%F{2}%b%F{3}|%F{1}%a%F{5}]%f '
